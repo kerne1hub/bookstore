e3cd0adc5082a3dea6aa64a7fa84c8f930f7fe1e
Kernel <kerstal104@gmail.com>
2020-04-01 12:36:47 +0600

    [+] Init Spring Boot & Configuring

---
+++ src/test/java/com/kernel/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kernel.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
