aa6fbdc1c92e481955f29e81a6181360352bc21c
Kernel <kerstal104@gmail.com>
2020-04-03 11:27:42 +0600

    [+] Created Angular project

--- src/main/java/com/kernel/bookstore/repository/BookRepository.java
+++ src/main/java/com/kernel/bookstore/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.kernel.bookstore.repository;
 
 import com.kernel.bookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
