b297bfb78d2501a6a8d985637fe4954288b65ed1
Kernel <kerstal104@gmail.com>
2020-04-06 10:57:10 +0600

    [+] Completed [Feature-4] Dynamic Search

--- angular-bookstore/src/app/services/book.service.ts
+++ angular-bookstore/src/app/services/book.service.ts
@@ -3,12 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from '../common/book';
 import { map } from 'rxjs/operators';
+import { BookCategory } from '../common/book-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
   private baseUrl = "http://localhost:8080/api/v1/books";
+  private categoryUrl = "http://localhost:8080/api/v1/book-category";
 
   constructor(private httpClient: HttpClient) { }
 
@@ -18,10 +20,22 @@ export class BookService {
       map(response => response._embedded.books)
     );
   }
+
+  getBookCategories(): Observable<BookCategory[]> {
+    return this.httpClient.get<GetResponseBookCategories>(this.categoryUrl).pipe(
+      map(response => response._embedded.bookCategory)
+    );
+  }
 }
 
 interface GetResponseBooks {
   _embedded: {
     books: Book[];
+  }
+}
+
+interface GetResponseBookCategories {
+  _embedded: {
+    bookCategory: BookCategory[];
   }
 }
--- src/main/java/com/kernel/bookstore/repository/BookRepository.java
+++ src/main/java/com/kernel/bookstore/repository/BookRepository.java
@@ -6,9 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @RestResource(path = "categoryid")
